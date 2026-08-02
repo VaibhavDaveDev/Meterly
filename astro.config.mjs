2abd7b7cea6322f7cc0bbd5f3ccb28cff952bc25
Vaibhav Dave <35380023+VaibhavDaveDev@users.noreply.github.com>
2026-08-02 12:27:32 +0530

    chore: enable sourcemaps in production for debugging

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,9 @@ export default defineConfig({
   }),
   integrations: [react(), tailwind()],
   vite: {
+    build: {
+      sourcemap: true,
+    },
     optimizeDeps: {
       include: ["picomatch"],
     },
